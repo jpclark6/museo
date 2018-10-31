8f7eca885869e6045c8e2a090e264182bd3fd2df
jpclark6 <jpclark6@gmail.com>
2018-10-31 09:22:16 -0600

    Adds curator tests

---
+++ test/curator_test.rb
@@ -0,0 +1,102 @@
+require 'minitest/autorun'
+require 'minitest/pride'
+require 'pry'
+require './lib/curator'
+
+class CuratorTest < Minitest::Test
+  def test_it_exists
+    curator = Curator.new
+    assert_instance_of Curator, curator
+  end
+
+  def test_it_has_default_empty_photographs
+    curator = Curator.new
+    assert_equal true, curator.photographs.empty?
+  end
+
+  def test_it_can_add_photographs
+    curator = Curator.new
+    photo_1 = {
+      id: "1",
+      name: "Rue Mouffetard, Paris (Boy with Bottles)",
+      artist_id: "1",
+      year: "1954"
+    }
+    photo_2 = {
+      id: "2",
+      name: "Moonrise, Hernandez",
+      artist_id: "2",
+      year: "1941"
+    }
+    curator.add_photograph(photo_1)
+    curator.add_photograph(photo_2)
+    expected = [photo_1, photo_2]
+    assert_equal expected, curator.photographs
+    assert_equal photo_1, curator.photographs.first
+    assert_equal "Rue Mouffetard, Paris (Boy with Bottles)", curator.photographs.first.name
+  end
+
+  def test_it_can_add_artists
+    curator = Curator.new
+    artist_1 = {
+      id: "1"
+      name: "Henri Cartier-Bresson",
+      born: "1908",
+      died: "2004",
+      country: "France"
+    }
+    artist_2 = {
+      id: "2",
+      name: "Ansel Adams",
+      born: "1902",
+      died: "1984",
+      country: "United States"
+    }
+    assert_equal true, curator.artists.empty?
+    curator.add_artist(artist_1)
+    curator.add_artist(artist_2)
+    expected = [artist_1, artist_2]
+    assert_equal expected, curator.artists
+    assert_equal artist_1, curator.artists.first
+  end
+
+  def test_it_can_find_artist_by_id
+    curator = Curator.new
+    artist_1 = {
+      id: "1"
+      name: "Henri Cartier-Bresson",
+      born: "1908",
+      died: "2004",
+      country: "France"
+    }
+    artist_2 = {
+      id: "2",
+      name: "Ansel Adams",
+      born: "1902",
+      died: "1984",
+      country: "United States"
+    }
+    curator.add_artist(artist_1)
+    curator.add_artist(artist_2)
+    assert_equal artist_2, curator.find_artist_by_id("2")
+  end
+
+  def test_it_can_find_photograph_by_id
+    curator = Curator.new
+    photo_1 = {
+      id: "1",
+      name: "Rue Mouffetard, Paris (Boy with Bottles)",
+      artist_id: "1",
+      year: "1954"
+    }
+    photo_2 = {
+      id: "2",
+      name: "Moonrise, Hernandez",
+      artist_id: "2",
+      year: "1941"
+    }
+    curator.add_photograph(photo_1)
+    curator.add_photograph(photo_2)
+    assert_equal photo_2, curator.find_photograph_by_id("2")
+  end
+end
