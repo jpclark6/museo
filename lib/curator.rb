647ca5ecd4421b4cc7a375e61d9c6fc677e24bbc
jpclark6 <jpclark6@gmail.com>
2018-10-31 09:50:49 -0600

    Finishes curator iteration 3

--- lib/curator.rb
+++ lib/curator.rb
@@ -22,4 +22,20 @@ class Curator
     @photographs.find { |photo| photo.id == photo_id }
   end
 
+  def find_photographs_by_artist(artist)
+    @photographs.find_all { |photo| photo.artist_id == artist.id }
+  end
+
+  def artists_with_multiple_photographs
+    @artists.find_all do |artist|
+      @photographs.count { |photo| photo.artist_id == artist.id } > 1
+    end
+  end
+
+  def photographs_taken_by_artist_from(country)
+    @photographs.find_all do |photo|
+      find_artist_by_id(photo.artist_id).country == country
+    end
+  end
+
 end
