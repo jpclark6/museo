00a1a3a9e3b751ea2767e641d5130b3a905c0667
jpclark6 <jpclark6@gmail.com>
2018-10-31 10:50:13 -0600

    Refactor and cleanup

--- lib/curator.rb
+++ lib/curator.rb
@@ -1,3 +1,7 @@
+require './lib/file_io'
+require './lib/photograph'
+require './lib/artist'
+
 class Curator
   attr_reader :photographs, :artists
 
@@ -32,10 +36,48 @@ class Curator
     end
   end
 
-  def photographs_taken_by_artist_from(country)
+  def photographs_taken_by_artists_from(country)
     @photographs.find_all do |photo|
       find_artist_by_id(photo.artist_id).country == country
     end
+  end
+
+  def load_photographs(file)
+    photo_data = FileIO.load_photographs(file)
+    photo_data.each do |photo_info|
+      add_photograph(Photograph.new(photo_info))
+    end
+  end
+
+  def load_artists(file)
+    artist_data = FileIO.load_artists(file)
+    artist_data.each do |artist_info|
+      add_artist(Artist.new(artist_info))
+    end
+  end
+
+  def photographs_taken_between_range(range)
+    @photographs.find_all { |photo| range.cover? photo.year.to_i }
+  end
+
+  def artists_photographs_by_age(artist)
+    age_and_photo_array = find_photographs_by_artist(artist).map do |photo|
+      [photo.year.to_i - artist.born.to_i, photo.name]
+    end
+    create_age_and_photo_name_hash(age_and_photo_array)
+
+  end
+
+  def create_age_and_photo_name_hash(age_photos)
+    age_photo_hash = {}
+    age_photos.each do |age, photo_name|
+      if age_photo_hash.has_key? age
+        age_photo_hash[age] = "'age_photo_hash[age]', '#{photo_name}'"
+      else
+        age_photo_hash[age] = photo_name
+      end
+    end
+    age_photo_hash
   end
 
 end
--- test/curator_test.rb
+++ test/curator_test.rb
@@ -51,9 +51,9 @@ class CuratorTest < Minitest::Test
     @curator.add_photograph(@photo_2)
     @curator.add_photograph(@photo_3)
   end
+
   def test_it_exists
-    curator = Curator.new
-    assert_instance_of Curator, curator
+    assert_instance_of Curator, @curator
   end
 
   def test_it_has_default_empty_photographs
@@ -62,104 +62,24 @@ class CuratorTest < Minitest::Test
   end
 
   def test_it_can_add_photographs
-    curator = Curator.new
-    attributes_1 = {
-      id: "1",
-      name: "Rue Mouffetard, Paris (Boy with Bottles)",
-      artist_id: "1",
-      year: "1954"
-    }
-    attributes_2 = {
-      id: "2",
-      name: "Moonrise, Hernandez",
-      artist_id: "2",
-      year: "1941"
-    }
-    photo_1 = Photograph.new(attributes_1)
-    photo_2 = Photograph.new(attributes_2)
-
-    curator.add_photograph(photo_1)
-    curator.add_photograph(photo_2)
-    expected = [photo_1, photo_2]
-    assert_equal expected, curator.photographs
-    assert_equal photo_1, curator.photographs.first
-    assert_equal "Rue Mouffetard, Paris (Boy with Bottles)", curator.photographs.first.name
+    expected = [@photo_1, @photo_2, @photo_3]
+    assert_equal expected, @curator.photographs
+    assert_equal @photo_1, @curator.photographs.first
+    assert_equal "Rue Mouffetard, Paris (Boy with Bottles)", @curator.photographs.first.name
   end
 
   def test_it_can_add_artists
-    curator = Curator.new
-    attributes_1 = {
-      id: "1",
-      name: "Henri Cartier-Bresson",
-      born: "1908",
-      died: "2004",
-      country: "France"
-    }
-    attributes_2 = {
-      id: "2",
-      name: "Ansel Adams",
-      born: "1902",
-      died: "1984",
-      country: "United States"
-    }
-    artist_1 = Artist.new(attributes_1)
-    artist_2 = Artist.new(attributes_2)
-
-    assert_equal true, curator.artists.empty?
-
-    curator.add_artist(artist_1)
-    curator.add_artist(artist_2)
-
-    expected = [artist_1, artist_2]
-    assert_equal expected, curator.artists
-    assert_equal artist_1, curator.artists.first
+    expected = [@artist_1, @artist_2]
+    assert_equal expected, @curator.artists
+    assert_equal @artist_1, @curator.artists.first
   end
 
   def test_it_can_find_artist_by_id
-    curator = Curator.new
-    attributes_1 = {
-      id: "1",
-      name: "Henri Cartier-Bresson",
-      born: "1908",
-      died: "2004",
-      country: "France"
-    }
-    attributes_2 = {
-      id: "2",
-      name: "Ansel Adams",
-      born: "1902",
-      died: "1984",
-      country: "United States"
-    }
-    artist_1 = Artist.new(attributes_1)
-    artist_2 = Artist.new(attributes_2)
-
-    assert_equal true, curator.artists.empty?
-    curator.add_artist(artist_1)
-    curator.add_artist(artist_2)
-    assert_equal artist_2, curator.find_artist_by_id("2")
+    assert_equal @artist_2, @curator.find_artist_by_id("2")
   end
 
   def test_it_can_find_photograph_by_id
-    curator = Curator.new
-    attributes_1 = {
-      id: "1",
-      name: "Rue Mouffetard, Paris (Boy with Bottles)",
-      artist_id: "1",
-      year: "1954"
-    }
-    attributes_2 = {
-      id: "2",
-      name: "Moonrise, Hernandez",
-      artist_id: "2",
-      year: "1941"
-    }
-    photo_1 = Photograph.new(attributes_1)
-    photo_2 = Photograph.new(attributes_2)
-
-    curator.add_photograph(photo_1)
-    curator.add_photograph(photo_2)
-    assert_equal photo_2, curator.find_photograph_by_id("2")
+    assert_equal @photo_2, @curator.find_photograph_by_id("2")
   end
 
   def test_it_can_find_photographs_by_artist
@@ -172,7 +92,7 @@ class CuratorTest < Minitest::Test
     assert_equal expected, @curator.artists_with_multiple_photographs
   end
 
-  def test_it_can_find_photographs_taken_by_artist_from_country
+  def test_it_can_find_photographs_taken_by_artists_from_country
     attributes_3 = {
       id: "3",
       name: "George Washington",
@@ -191,7 +111,39 @@ class CuratorTest < Minitest::Test
     @curator.add_artist(artist_3)
     @curator.add_photograph(photo_4)
     expected = [@photo_2, @photo_3, photo_4]
-    assert_equal expected, @curator.photographs_taken_by_artist_from("United States")
-    assert_equal [], @curator.photographs_taken_by_artist_from("Moon")
+    assert_equal expected, @curator.photographs_taken_by_artists_from("United States")
+    assert_equal [], @curator.photographs_taken_by_artists_from("Moon")
+  end
+
+  def test_it_can_load_photographs_from_file
+    curator = Curator.new
+    photo_file = './data/photographs.csv'
+    curator.load_photographs(photo_file)
+
+    artist_file = './data/artists.csv'
+    curator.load_artists(artist_file)
+
+    assert_equal 4, curator.photographs.length
+    assert_equal 6, curator.artists.length
+  end
+
+  def test_it_can_find_photographs_taken_between_range
+    range = 1941..1945
+    expected = [@photo_2, @photo_3]
+    assert_equal expected, @curator.photographs_taken_between_range(range)
+  end
+
+  def test_it_can_find_artists_photographs_by_age
+    curator = Curator.new
+
+    photo_file = './data/photographs.csv'
+    curator.load_photographs(photo_file)
+
+    artist_file = './data/artists.csv'
+    curator.load_artists(artist_file)
+    diane_arbus = curator.find_artist_by_id("3")
+    expected ={44=>"Identical Twins, Roselle, New Jersey", 39=>"Child with Toy Hand Grenade in Central Park"}
+
+    assert_equal expected, curator.artists_photographs_by_age(diane_arbus)
   end
 end
